4cac5c0f38db001b4c8b746a2178b1ec2f7ca41c
oniflestari <onifahmilestari@gmail.com>
2022-07-19 13:40:38 +0700

    Tugas 24 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas24.js
@@ -0,0 +1,12 @@
+var arr1 = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+string1 = arr1.toString();
+
+var arr2 = arr1.sort();
+string2 = arr2.toString();
+
+var arr3 = arr2.reverse();
+string3 = arr3.toString();
+
+console.log('Sebelumnya : ', string1);
+console.log('Ascending : ', string2);
+console.log('Descending :', string3);
